df0e2c23fe15b046f6196e4763eee958ee363b6e
Kumail Naqvi <kumailmn@gmail.com>
2018-10-21 05:51:11 -0400

    Big fix

--- front/src/store.js
+++ front/src/store.js
@@ -5,7 +5,15 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    showBox: false,
+    currentStory: '',
+    stories: [],
+    comics: [],
+    faces: [],
+    faceMap: 'http://40.117.32.177:8080/face?text=',
+    currentImageIndex: 0,
+    originalImageSizes: [],
+    currentImageSize: {},
   },
   mutations: {
 
